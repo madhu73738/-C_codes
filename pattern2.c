e756720ccc3b6b8305da30c034478be3ddcfeb22
Madhu Hazra <108191636+madhu73738@users.noreply.github.com>
2023-05-30 20:21:39 +0530

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main() 
+{
+int i,j,num,a,b;
+
+num=5;
+
+a,b=1,0;
+for(i=1;i<num+1;i++)
+{
+    
+    for(j=1;j<i+1;j++)
+    {   
+        printf("%d ",j);
+    }
+    
+printf("\n");
+
+
+}
+
+    return 0;
+}
